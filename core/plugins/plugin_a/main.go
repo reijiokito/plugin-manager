88060c1f2e5c1798f1bbc23c165001fda06ee994
reijiokito <romeothecong@gmail.com>
2023-04-26 11:26:56 +0700

    update

--- core/plugins/plugin_a/main.go
+++ core/plugins/plugin_a/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
 	"github.com/reijiokito/go-pdk"
 	"github.com/reijiokito/plugin-manager/core/plugins/plugin_a/event"
 	"github.com/reijiokito/plugin-manager/core/plugins/plugin_a/service"
-	proto2 "github.com/reijiokito/plugin-manager/core/plugins/plugin_b/proto"
 )
 
 func Access(pdk *go_pdk.PDK) {
@@ -14,10 +12,6 @@ func Access(pdk *go_pdk.PDK) {
 	go_pdk.RegisterSubject("manager.handshake", event.HandShakeHandler)
 
 	go_pdk.RegisterSubject("kkk", service.Hi)
-
-	for i := 0; i < 500; i++ {
-		go pdk.Chan.PostEvent("hkt", &proto2.HelloB{Name: fmt.Sprintf("hkt con chim non from plug in A- HKT - %v", i)})
-	}
 
 	pdk.Start()
 }
--- core/plugins/plugin_b/main.go
+++ core/plugins/plugin_b/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	go_pdk "github.com/reijiokito/go-pdk"
+	"github.com/reijiokito/plugin-manager/core/plugins/plugin_a/proto"
 	proto2 "github.com/reijiokito/plugin-manager/core/plugins/plugin_b/proto"
 	"github.com/reijiokito/plugin-manager/core/plugins/plugin_b/service"
 )
@@ -10,11 +11,11 @@ import (
 func Access(pdk *go_pdk.PDK) {
 	go_pdk.RegisterService(pdk.Nats.Connection, "/user/newB", service.CreateNew)
 
-	go_pdk.RegisterSubject("hkt", service.Hi)
-
 	for i := 0; i < 2000; i++ {
 		go pdk.Chan.PostEvent("kkk", &proto2.HelloB{Name: fmt.Sprintf("kkk from plugin B - %v", i)})
 	}
+
+	pdk.Nats.PostEvent("manager.handshake", &proto.Handshake{Name: "HIHI"})
 
 	pdk.Start()
 }
